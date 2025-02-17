ee01d7872ad20072f7ed1afe35cc69c7010d17dd
Belal Abu Hatab <belalabwhatb2@gmail.com>
2025-02-17 12:27:20 +0200

    feat(Modify): modify user migration , api route , auth config , cors config and User Model

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'user',
         'passwords' => 'users',
     ],
 
@@ -36,7 +36,15 @@ return [
     */
 
     'guards' => [
-        'web' => [
+//        'web' => [
+//            'driver' => 'session',
+//            'provider' => 'users',
+//        ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+        'user' => [
             'driver' => 'session',
             'provider' => 'users',
         ],
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::post('login', 'AuthController@login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('current-user', [AuthController::class, 'me'])->name('current-user');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
+    Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('reset-password');
+    Route::put('change-password', [AuthController::class, 'changePassword'])->name('change-password');
+    Route::post('initiateEmailChange', [AuthController::class, 'initiateEmailChange'])->name('initiateEmailChange');
+    Route::post('confirmEmailChange', [AuthController::class, 'confirmEmailChange'])->name('confirmEmailChange');
+    Route::post('initiatePhoneChange', [AuthController::class, 'initiatePhoneChange'])->name('initiatePhoneChange');
+    Route::post('confirmPhoneChange', [AuthController::class, 'confirmPhoneChange'])->name('confirmPhoneChange');
 });
+//Route::post('me', 'AuthController@me');
+
